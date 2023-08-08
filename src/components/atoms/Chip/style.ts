66e957a6e1909581c30f99b6e815225acfccd3ef
rainleee <rainlee4401@gmail.com>
2023-08-08 18:03:53 +0900

    feat(#16): add atoms component Chip and stories

---
+++ src/components/atoms/Chip/style.ts
@@ -0,0 +1,26 @@
+import { styled } from "@/styles/stitches.config";
+
+export const ChipStyled = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  color: "white",
+  fontSize: "12px",
+  fontWeight: "600",
+  padding: "7px 11px",
+  borderRadius: "100px",
+  width: "max-content",
+  variants: {
+    color: {
+      blue: { backgroundColor: "#6165FF" },
+      yellow: { backgroundColor: "#FFC061" },
+      lightYellow: { color: "#9C7C08", backgroundColor: "#F9ECBE" },
+      gray: { color: "#686A6C", backgroundColor: "#EFF0F1" },
+      red: { color: "#FF2326", backgroundColor: "#FFEBEB" },
+      green: { color: "#038235", backgroundColor: "#D8F7E6" },
+    },
+  },
+  defaultVariants: {
+    color: "blue",
+  },
+});
---
+++ src/components/atoms/Chip/type.ts
@@ -0,0 +1,9 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
+
+import { ChipStyled } from './style';
+
+export interface ChipProps extends ComponentPropsWithoutRef<typeof ChipStyled> {
+  children: ReactNode;
+  color?: ChipColorType;
+}
+export type ChipColorType = 'blue' | 'yellow' | 'lightYellow' | 'gray' | 'red' | 'green';
