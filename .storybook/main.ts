c1116f3284cc77cb4079075fbe35a4b3305a8539
rainleee <rainlee4401@gmail.com>
2023-08-08 17:53:43 +0900

    feat(#17): storybook 절대경로를 인식하게 세팅한다. Closes #17

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -11,7 +13,16 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
+  },
+  webpackFinal: async (config, { configType }) => {
+    if (config.resolve)
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+
+    return config;
   },
 };
 export default config;
