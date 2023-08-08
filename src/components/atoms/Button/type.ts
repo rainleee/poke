d5418ee29ba019df6ed285bec82b6b9a09362476
rainleee <rainlee4401@gmail.com>
2023-08-08 18:03:22 +0900

    feat(#16): add atoms component Button and stories

---
+++ src/components/atoms/Button/type.ts
@@ -0,0 +1,7 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
+
+import { ButtonStyled } from './style';
+
+export interface ButtonProps extends ComponentPropsWithoutRef<typeof ButtonStyled> {
+  children: ReactNode;
+}
