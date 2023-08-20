84d4501dc724f4a76dd1de10519bfbc5641a62ed
rainleee <rainlee4401@gmail.com>
2023-08-20 23:02:44 +0900

    feat: add graphql loader lib

--- next.config.js
+++ next.config.js
@@ -9,6 +9,17 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
+  webpack(config, options) {
+    config.experiments.topLevelAwait = true;
+
+    config.module.rules.push({
+      exclude: /node_modules/,
+      test: /\.graphql$/,
+      use: [options.defaultLoaders.babel, { loader: "graphql-tag/loader" }],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
